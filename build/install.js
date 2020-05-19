93e3a10572ce8d7a75cb4f71d00caf03b304def5
roydukkey <contact@changelog.me>
2020-05-19 15:36:55 -0400

    Add install script to ensure sass-list has correct path

---
+++ build/install.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+
+const requiredPackage = './node_modules/sass-list';
+const fallbackPackage = '../sass-list';
+
+// Check to see if the required sass-list module exists at the required location.
+// This protect the user from need to configure their sass compiler to find this package.
+fs.access(requiredPackage, fs.constants.R_OK, (err) => {
+	if (err) {
+
+		fsExtra.copy(fallbackPackage, requiredPackage, (err) => {
+			if (err) {
+				return console.error(err);
+			}
+		});
+
+	}
+});
